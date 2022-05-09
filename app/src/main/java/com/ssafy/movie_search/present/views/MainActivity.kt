404af31ac5e1e2e092cc4f03c467464bba6e50d2
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-10 03:55:53 +0900

    #2 feat: :sparkles: MainActivity에 frameLayout 추가

--- app/src/main/java/com/ssafy/movie_search/present/views/MainActivity.kt
+++ app/src/main/java/com/ssafy/movie_search/present/views/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.ssafy.movie_search.present.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.andback.pocketfridge.present.config.BaseActivity
 import com.ssafy.movie_search.R
+import com.ssafy.movie_search.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //초기 실행화면 설정
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fl_main)
+
+        if(currentFragment == null){
+            val fragment = MovieFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fl_main, fragment)
+                .commit()
+        }
     }
 }
