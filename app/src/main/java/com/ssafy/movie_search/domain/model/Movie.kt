e6da972126a85aa687f8313f56b0e36f77ebb6a1
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-10 03:57:46 +0900

    #2 feat: :sparkles: MovieAdapter 구현 및 Movie dto 작성

---
+++ app/src/main/java/com/ssafy/movie_search/domain/model/Movie.kt
@@ -0,0 +1,8 @@
+package com.ssafy.movie_search.domain.model
+
+data class Movie(
+    val imageUrl: String?,
+    val title: String,
+    val year: String,
+    val rating: String
+)
---
+++ app/src/main/java/com/ssafy/movie_search/present/views/MovieAdapter.kt
@@ -0,0 +1,51 @@
+package com.ssafy.movie_search.present.views
+
+import android.animation.Animator
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.movie_search.databinding.ItemMovieListBinding
+import com.ssafy.movie_search.domain.model.Movie
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+    private lateinit var itemClickListener: ItemClickListener
+    private val movieList: ArrayList<Movie> = ArrayList()
+
+    inner class MovieViewHolder(private val binding: ItemMovieListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindInfo(movie: Movie) {
+            binding.movie = movie
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        var binding = ItemMovieListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.apply {
+            bindInfo(movieList[position])
+
+            itemView.setOnClickListener{
+                itemClickListener.onClick(movieList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = movieList.size
+
+    fun setList(list: List<Movie>) {
+        movieList.clear()
+        movieList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    interface ItemClickListener {
+        fun onClick(recipeId: Movie)
+    }
+
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+}
